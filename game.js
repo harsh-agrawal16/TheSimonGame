b173d4dcd3e8194d9b559c7216c00658a2d316f8
harsh-agrawal16 <agrwl.harsh@gmail.com>
2020-07-13 14:58:03 +0530

    Second Commit

---
+++ game.js
@@ -0,0 +1,75 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level;
+var started = false;
+
+
+//This function helps restart the game whenever the pattern breaks down.
+function restartGame(){
+  started = false;
+  gamePattern = [];
+  userClickedPattern = [];
+  $("h1").text("Press any key to start");
+}
+
+//This function plays a sound for a particular button , either when clicked or nextSequence is called.
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+//This helps animate a flash on a button.
+function buttonAnimation(name) {
+  $("." + name).addClass("pressed");
+  setTimeout(function() {
+    $("." + name).removeClass("pressed");
+  }, 100)
+}
+
+// This function adds a new color to the sequence.
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("h1").text("Level " + level);
+
+  var randomNumber = Math.floor((Math.random() * 4));
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+
+$(".btn").click(function(event) {
+  var userChosenColor = event.target.id;
+  userClickedPattern.push(userChosenColor);
+  buttonAnimation(userChosenColor);
+  playSound(userChosenColor);
+
+  var len = userClickedPattern.length;
+  if(userClickedPattern[len - 1] != gamePattern[len - 1]){
+    console.log("Game Over");
+    restartGame();
+  }
+
+  else if(len === gamePattern.length){
+    setTimeout(function(){
+      nextSequence();
+    }, 1000);
+  }
+})
+
+
+//This listens to the document and gets triggered whenever a key is pressed.
+$(document).keydown(function(event) {
+  level = 0;
+  if (started === false) {
+    started = true;
+    userClickedPattern = [];
+    gamePattern = [];
+    nextSequence();
+    $("h1").text("Level " + level);
+  }
+});
